be99f55b254770ea1e59836ded514c920cd3f874
se7t <kacper.kochan@protonmail.com>
2023-06-08 10:39:11 +0200

    Refactor menu

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,18 +1,15 @@
 const Menu = () => {
+  const menuItems = ["Products", "Solutions", "Resources", "Pricing"];
+
   return (
     <ul className="hidden justify-center gap-8 align-baseline font-medium xl:ml-28 xl:flex">
-      <li className="cursor-pointer">
-        <a>Products</a>
-      </li>
-      <li className="cursor-pointer">
-        <a>Solutions</a>
-      </li>
-      <li className="cursor-pointer">
-        <a>Resources</a>
-      </li>
-      <li className="cursor-pointer">
-        <a>Pricing</a>
-      </li>
+      {menuItems.map((item, index) => {
+        return (
+          <li key={index} className="cursor-pointer">
+            {item}
+          </li>
+        );
+      })}
     </ul>
   );
 };
